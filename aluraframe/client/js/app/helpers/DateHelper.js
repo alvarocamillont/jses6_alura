ab2630c2b1a9afdbc5ad5e325d06e01865e69c77
Alvaro <alvaro.camilloneto@gmail.com>
2017-06-22 23:12:12 -0300

    Validação com Expressão Regular e utilização de Template String

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -4,6 +4,8 @@ class DateHelper {
   }
 
   static textoParaData (texto) {
+    if (!/\d{4}-\d{2}-\d{2}/.test(texto)) { throw new Error('Deve estar no formato aaaa-mm-dd') }
+
     return new Date(
             ...texto
                 .split('-')
@@ -12,8 +14,6 @@ class DateHelper {
   }
 
   static dataParaTexto (data) {
-    return data.getDate() +
-        '/' + (data.getMonth() + 1) +
-        '/' + data.getFullYear()
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} `
   }
 }
