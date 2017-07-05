87e6baab4861926bd79cf2ffde2af18e8d03babb
Alvaro <alvaro.camilloneto@gmail.com>
2017-07-04 23:29:23 -0300

    Refatoração da atualização de modelos, criação da função apagar

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,7 +4,7 @@ class NegociacaoController {
     this._inputData = $('#data')
     this._inputQuantidade = $('#quantidade')
     this._inputValor = $('#valor')
-    this._listaNegociacoes = new ListaNegociacoes()
+    this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model))
     this._negociacoesView = new NegociacoesView($('#negociacoesView'))
     this._negociacoesView.update(this._listaNegociacoes)
     this._mensagem = new Mensagem()
@@ -15,7 +15,6 @@ class NegociacaoController {
   adiciona (event) {
     event.preventDefault()
     this._listaNegociacoes.adiciona(this._criaNegociacao())
-    this._negociacoesView.update(this._listaNegociacoes)
     this._mensagem.texto = 'Negociação adicionada com sucesso'
     this._mensagemView.update(this._mensagem)
     this._limpaFormulario()
@@ -34,5 +33,12 @@ class NegociacaoController {
     this._inputValor.value = 0.0
 
     this._inputData.focus()
+  }
+
+  apagar () {
+    this._listaNegociacoes.limpar()
+    this._mensagem.texto = 'Lista excluída'
+    this._mensagemView.update(this._mensagem)
+    this._limpaFormulario()
   }
 }
