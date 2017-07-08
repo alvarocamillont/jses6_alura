c07346d768fddcc98fc1ce95ac759f7e044c1858
Alvaro <alvaro.camilloneto@gmail.com>
2017-07-08 00:10:18 -0300

    Simplificando o código

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,20 +5,15 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade')
     this._inputValor = $('#valor')
 
-    let self = this
-
-    this._negociacoesView = new NegociacoesView($('#negociacoesView'))
-
     this._listaNegociacoes = new Bind(
                 new ListaNegociacoes(),
-                this._negociacoesView,
-                ['adiciona', 'esvazia'])
+                new NegociacoesView($('#negociacoesView')),
+                'adiciona', 'esvazia')
 
-    this._mensagemView = new MensagemView($('#mensagemView'))
     this._mensagem = new Bind(
         new Mensagem(),
-        self._mensagemView,
-        ['texto'])
+        new MensagemView($('#mensagemView')),
+        'texto')
   }
 
   adiciona (event) {
