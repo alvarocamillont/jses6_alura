ca4f15d7c1cc5b20a2e55ffb331da3e61f0adcb5
Alvaro <alvaro.camilloneto@gmail.com>
2017-07-10 23:22:56 -0300

    Convertendo para promisse

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -47,14 +47,14 @@ class NegociacaoController {
   importaNegociacoes () {
     let service = new NegociacaoService()
 
-    service.obterNegociacoesDaSemana((erro, negociacoes) => {
-      if (erro) {
-        this._mensagem.texto = erro
-        return
-      }
-
-      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-      this._mensagem.texto = 'Negociações importadas com sucesso'
-    })
+    let promise = service.obterNegociacoesDaSemana()
+    promise
+        .then(negociacoes => {
+          negociacoes.forEach(negociacao => {
+            this._listaNegociacoes.adiciona(negociacao)
+          })
+          this._mensagem.texto = 'Negociações importadas com sucesso'
+        })
+    .catch(erro => this._mensagem.texto = erro)
   }
 }
--- aluraframe/client/js/app/services/NegociacaoService.js
+++ aluraframe/client/js/app/services/NegociacaoService.js
@@ -7,33 +7,35 @@ xhr.readyState
 3: processando requisição.
 4: requisição concluída e a resposta esta pronta.
 */
-  _obterNegociacoes (cb, url, msgerro) {
-    let xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          cb(null, JSON.parse(xhr.responseText)
+  _obterNegociacoes (url, msgerro) {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest()
+      xhr.open('GET', url)
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.responseText)
                         .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))
-        } else {
-          console.log(xhr.responseText)
-          cb(msgerro, null)
+          } else {
+            console.log(xhr.responseText)
+            reject(msgerro)
+          }
         }
       }
-    }
 
-    xhr.send()
+      xhr.send()
+    })
   }
 
-  obterNegociacoesDaSemana (cb) {
-    this._obterNegociacoes(cb, 'negociacoes/semana', 'Não foi possível obter as negociações da semana')
+  obterNegociacoesDaSemana () {
+    return this._obterNegociacoes('negociacoes/semana', 'Não foi possível obter as negociações da semana')
   }
 
-  obterNegociacoesDaSemanaAnterior (cb) {
-    this._obterNegociacoes(cb, 'negociacoes/anterior', 'Não foi possível obter as negociações da semana anterior')
+  obterNegociacoesDaSemanaAnterior () {
+    return this._obterNegociacoes('negociacoes/anterior', 'Não foi possível obter as negociações da semana anterior')
   }
 
   obterNegociacoesDaSemanaRetrasada (cb) {
-    this._obterNegociacoes(cb, 'negociacoes/retrasada', 'Não foi possível obter as negociações da semana Retrasada')
+    return this._obterNegociacoes('negociacoes/retrasada', 'Não foi possível obter as negociações da semana Retrasada')
   }
 }
