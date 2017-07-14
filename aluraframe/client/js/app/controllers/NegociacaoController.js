6c260c8b99a9cb4eeda7a3693e7bb8d94cec1e57
Alvaro <alvaro.camilloneto@gmail.com>
2017-07-13 23:48:50 -0300

    Incluidas novas lições sobre o padrãp DAO

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -19,16 +19,27 @@ class NegociacaoController {
 
   adiciona (event) {
     event.preventDefault()
-    this._listaNegociacoes.adiciona(this._criaNegociacao())
-    this._mensagem.texto = 'Negociação adicionada com sucesso'
-    this._limpaFormulario()
+    ConnectionFactory
+            .getConnection()
+            .then(conexao => {
+              let negociacao = this._criaNegociacao()
+
+              new NegociacaoDao(conexao)
+                    .adiciona(negociacao)
+                    .then(() => {
+                      this._listaNegociacoes.adiciona(negociacao)
+                      this._mensagem.texto = 'Negociação adicionada com sucesso'
+                      this._limpaFormulario()
+                    })
+            })
+        .catch(erro => this._mensagem.texto = erro)
   }
 
   _criaNegociacao () {
     return new Negociacao(
-        DateHelper.textoParaData(this._inputData.value),
-        this._inputQuantidade.value,
-        this._inputValor.value)
+       DateHelper.textoParaData(this._inputData.value),
+       parseInt(this._inputQuantidade.value),
+       parseFloat(this._inputValor.value))
   }
 
   _limpaFormulario () {
